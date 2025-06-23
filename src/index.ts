c9da6a192871e44e152838a595923b0576214248
Maksiks <maksiksking@gmail.com>
2025-06-23 18:17:39 +0300

    fix: added CommonJS export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import markdownIt, {Options, Renderer, Token} from "markdown-it";
 
-'use strict';
-
-export const cap_plugin = (md: markdownIt): void => {
+const markdownItImageCaption = (md: markdownIt): void => {
     const old = md.renderer.rules.image;
 
     md.renderer.rules.image = (tokens: Token[], idx: number, options: Options, env: any, self: Renderer): string => {
@@ -11,13 +9,20 @@ export const cap_plugin = (md: markdownIt): void => {
             let title = attrs[2][1];
             let src = attrs[0][1];
             let alt = tokens[idx].content;
-            return `<figure><img src="${src}" alt="${alt}" title="${title}" /><figcaption>${title}</figcaption></figure>`
+
+            const imgTag = `<img src="${src}" alt="${alt}" ${title !== ':::nocaption' ? ` title="${title}"` : ''} />`;
+
+            return title !== ':::nocaption'
+                ? `<figure>${imgTag}<figcaption>${title}</figcaption></figure>`
+                : `<figure>${imgTag}</figure>`;
         }
         if (old) {
             return old(tokens, idx, options, env, self);
         } else {
-            console.warn('@maksiks/markdown-it-image-caption: original rendered doesn\'t exist, something went horribly wrong.');
+            console.warn('@maksiks/markdown-it-image-caption: original renderer doesn\'t exist, something went horribly wrong.');
             return '';
         }
     }
 };
+
+export = markdownItImageCaption;
