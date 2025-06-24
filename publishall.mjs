2237e2d1c103f91e313fddb1803456736006d857
Maksiks <maksiksking@gmail.com>
2025-06-24 21:22:44 +0300

    chore: clean working tree

--- publishall.mjs
+++ publishall.mjs
@@ -2,10 +2,13 @@ import fs from "fs";
 import path from "path";
 import 'dotenv/config';
 import { execSync } from "child_process";
+import {configDotenv} from "dotenv";
+
+// the script works but my token is being wrongled, idk just did it manually
 
 execSync("pnpm run build", { stdio: "inherit" });
 
-execSync("pnpm publish", { stdio: "inherit" });
+execSync("pnpm publish --no-git-checks", { stdio: "inherit" });
 
 const tmp = ".gh-publish-tmp";
 fs.rmSync(tmp, { recursive: true, force: true });
@@ -19,7 +22,7 @@ console.log(pkg);
 
 fs.writeFileSync(`${tmp}/package.json`, JSON.stringify(pkg, null, 2));
 
-execSync(`cd ${tmp} && pnpm publish`, { stdio: "inherit" });
+execSync(`cd ${tmp} && pnpm publish --no-git-checks`, { stdio: "inherit" });
 
 fs.rmSync(tmp, { recursive: true, force: true });
 
