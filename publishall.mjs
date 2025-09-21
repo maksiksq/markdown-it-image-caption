74fa0352c6df7f6c41fd4b4c364f3173de31bbe8
Maksiks <maksiksking@gmail.com>
2025-09-21 10:51:43 +0300

    chore: squashing the heck of it

--- publishall.mjs
+++ publishall.mjs
@@ -4,8 +4,6 @@ import 'dotenv/config';
 import { execSync } from "child_process";
 import {configDotenv} from "dotenv";
 
-// the script works but my token is being wrongled, idk why, so just did it manually
-
 execSync("pnpm run build", { stdio: "inherit" });
 
 execSync("pnpm publish --no-git-checks", { stdio: "inherit" });
